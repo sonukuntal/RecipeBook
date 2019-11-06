585b603ceb417130224ba5b76b4c971fffe2456f
sonukuntal <sonu.savera.kuntal@gmail.com>
2019-11-06 16:04:43 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -19,7 +19,7 @@ import { ErrorpageComponent } from './errorpage/errorpage.component';
 import { RecipeStartComponent } from './recipies/recipe-start/recipe-start.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,AppRoutingModule],
+  imports:      [ BrowserModule, FormsModule,AppRoutingModule,ReactiveFormsModule],
   declarations: [ AppComponent, RecipiesComponent, RecipeListComponent, ShoopingListComponent, RecipeItemComponent, ShoopingEditComponent, HeaderComponent, RecipeDetailComponent,DropdownDirective, RecipeEditComponent, ErrorpageComponent, RecipeStartComponent],
   bootstrap:    [ AppComponent ],
   providers: [ShoopingListService,RecipiesService]
--- src/app/recipies/recipe-edit/recipe-edit.component.ts
+++ src/app/recipies/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeEditComponent implements OnInit {
 
+  recipeform:FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.recipeform=new FormGroup({
+     Name:new FormControl(null,Validators.required),
+     Description:new FormControl(null,Validators.required),
+     Ingredients:new FormControl(null,Validators.required)
+    })
+  }
+
+  onsubmitRecipe()
+  {
+    
   }
 
 }
