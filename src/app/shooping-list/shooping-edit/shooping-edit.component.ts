f31111c3dc64f6907c450a0b06ee594e10f68009
sonukuntal <sonu.savera.kuntal@gmail.com>
2019-11-06 14:36:44 +0530

    1 file modified

--- src/app/shooping-list/shooping-edit/shooping-edit.component.ts
+++ src/app/shooping-list/shooping-edit/shooping-edit.component.ts
@@ -1,45 +1,57 @@
-import { Component, OnInit,ElementRef,ViewChild,OnDestroy } from '@angular/core';
-import {ingredient} from '../../shared/ingredient.model';
-import {ShoopingListService} from '../shooping-list.service';
-import {NgForm} from '@angular/forms';
-import {Subscription} from 'rxjs';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  OnDestroy
+} from "@angular/core";
+import { ingredient } from "../../shared/ingredient.model";
+import { ShoopingListService } from "../shooping-list.service";
+import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-shooping-edit',
-  templateUrl: './shooping-edit.component.html',
-  styleUrls: ['./shooping-edit.component.css']
+  selector: "app-shooping-edit",
+  templateUrl: "./shooping-edit.component.html",
+  styleUrls: ["./shooping-edit.component.css"]
 })
-export class ShoopingEditComponent implements OnInit,OnDestroy {
-  @ViewChild('f',{static:true}) editform:NgForm;
-   subsc:Subscription;
-   edititem=false;
-   editindex:number;
-   edititemform:ingredient;
-    
-  constructor(private shoopingListService:ShoopingListService) { }
+export class ShoopingEditComponent implements OnInit, OnDestroy {
+  @ViewChild("f", { static: true }) editform: NgForm;
+  subsc: Subscription;
+  edititem = false;
+  editindex: number;
+  edititemform: ingredient;
+
+  constructor(private shoopingListService: ShoopingListService) {}
 
   ngOnInit() {
-   this.subsc= this.shoopingListService.editingredient.subscribe(
-     (index:number)=>{
-       this.editindex=index;
-       this.edititem=true;
-       this.edititemform=this.shoopingListService.getingredientbyid(index);
-       this.editform.setValue({
-         name:this.edititemform.name,
-         amount:this.edititemform.amount
-       })
+    this.subsc = this.shoopingListService.editingredient.subscribe(
+      (index: number) => {
+        this.editindex = index;
+        this.edititem = true;
+        this.edititemform = this.shoopingListService.getingredientbyid(index);
+        this.editform.setValue({
+          name: this.edititemform.name,
+          amount: this.edititemform.amount
+        });
       }
-   )
+    );
   }
 
-  onAddIngredients(form:NgForm)
-  {
-    const values=form.value;
-    this.shoopingListService.addnewingredient(new ingredient(values.name,values.amount));
+  onAddIngredients(form: NgForm) {
+    const values = form.value;
+    if (this.edititem) {
+      this.shoopingListService.updateingredients(
+        this.editindex,
+        this.edititemform
+      );
+    } else {
+      this.shoopingListService.addnewingredient(
+        new ingredient(values.name, values.amount)
+      );
+    }
   }
-  ngOnDestroy()
-  {
+  ngOnDestroy() {
     this.subsc.unsubscribe();
   }
-
 }
