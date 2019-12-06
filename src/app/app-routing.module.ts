648da87d812d3ea2a91383168061b97e6c07f169
sonukuntal <sonu.savera.kuntal@gmail.com>
2019-12-06 13:47:55 +0530

    1 file modified, 1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,46 +1,60 @@
-import { NgModule,Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {Routes,RouterModule} from '@angular/router';
-import { AppComponent } from './app.component';
-import { RecipiesComponent } from './recipies/recipies.component';
-import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
-import { ShoopingListComponent } from './shooping-list/shooping-list.component';
-import { RecipeItemComponent } from './recipies/recipe-list/recipe-item/recipe-item.component';
-import { ShoopingEditComponent } from './shooping-list/shooping-edit/shooping-edit.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipies/recipe-edit/recipe-edit.component';
-import { ErrorpageComponent } from './errorpage/errorpage.component';
-import { RecipeStartComponent } from './recipies/recipe-start/recipe-start.component';
-import {RecipiesResolverService} from './recipies/recipies-resolver.service';
-import {AuthComponent} from './auth/auth.component';
+import { NgModule, Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { RecipiesComponent } from "./recipies/recipies.component";
+import { RecipeListComponent } from "./recipies/recipe-list/recipe-list.component";
+import { ShoopingListComponent } from "./shooping-list/shooping-list.component";
+import { RecipeItemComponent } from "./recipies/recipe-list/recipe-item/recipe-item.component";
+import { ShoopingEditComponent } from "./shooping-list/shooping-edit/shooping-edit.component";
+import { HeaderComponent } from "./header/header.component";
+import { RecipeDetailComponent } from "./recipies/recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipies/recipe-edit/recipe-edit.component";
+import { ErrorpageComponent } from "./errorpage/errorpage.component";
+import { RecipeStartComponent } from "./recipies/recipe-start/recipe-start.component";
+import { RecipiesResolverService } from "./recipies/recipies-resolver.service";
+import { AuthComponent } from "./auth/auth.component";
+import {AuthGuard} from './auth/auth.guard';
 
-
-const appRoutes:Routes=[
-  {path:'', redirectTo:'/recipies',pathMatch:'full'},
-  {path:'recipies',component:RecipiesComponent, children:[
-    {path:'', component:RecipeStartComponent},
-    {path:'new',component:RecipeEditComponent},
-    {path:':id',component:RecipeDetailComponent,resolve:[RecipiesResolverService]},
-    {path:':id/edit',component:RecipeEditComponent,resolve:[RecipiesResolverService]},
-    {path:':list',component:RecipeListComponent,children:[
-      {path:':id/:item',component:RecipeItemComponent}
-    ]},
-  ]},
-  {path:'auth', component:AuthComponent},
-  {path:'shoopinglist',component:ShoopingListComponent,children:[
-   {path:':id/:edit',component:ShoopingEditComponent}
-  ]},
-  {path:'not-found',component:ErrorpageComponent},
-  {path:'**',redirectTo:'/not-found',pathMatch:'full'},
-]
+const appRoutes: Routes = [
+  { path: "", redirectTo: "/recipies", pathMatch: "full" },
+  {
+    path: "recipies",
+    component: RecipiesComponent,
+    canActivate:[AuthGuard],
+    children: [
+      { path: "", component: RecipeStartComponent },
+      { path: "new", component: RecipeEditComponent },
+      {
+        path: ":id",
+        component: RecipeDetailComponent,
+        resolve: [RecipiesResolverService]
+      },
+      {
+        path: ":id/edit",
+        component: RecipeEditComponent,
+        resolve: [RecipiesResolverService]
+      },
+      {
+        path: ":list",
+        component: RecipeListComponent,
+        children: [{ path: ":id/:item", component: RecipeItemComponent }]
+      }
+    ]
+  },
+  { path: "auth", component: AuthComponent },
+  {
+    path: "shoopinglist",
+    component: ShoopingListComponent,
+    children: [{ path: ":id/:edit", component: ShoopingEditComponent }]
+  },
+  { path: "not-found", component: ErrorpageComponent },
+  { path: "**", redirectTo: "/not-found", pathMatch: "full" }
+];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(appRoutes)
-  ],
-  exports:[RouterModule],
+  imports: [CommonModule, RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
   declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { map, tap,take } from "rxjs/operators";
+import { Router,UrlTree } from "@angular/router";
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    router: RouterStateSnapshot
+  ): boolean | UrlTree | Promise<boolean>|UrlTree | Observable<boolean>|UrlTree {
+    return this.authService.user.pipe(
+      take(1),
+      map(user => {
+        const isAuth= !!user;
+        if (isAuth) {
+         return true;
+        }
+        return this.router.createUrlTree(['/auth']);
+      })
+    );
+  }
+}
