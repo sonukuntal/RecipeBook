b24579a20186f178d9ccd5c4e107ea6d0584a000
sonukuntal <sonu.savera.kuntal@gmail.com>
2019-12-23 12:21:34 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from "@angular/core";
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
+import {StoreModule} from '@ngrx/store';
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { ErrorpageComponent } from "./errorpage/errorpage.component";
 import {SharedModule} from './shared/shared.module';
 import {CoreModule} from './core.module';
+import {ShoopingListReducer} from './shooping-list/Store/shooping-list.reducer';
+
 
 
 @NgModule({
@@ -15,7 +19,8 @@ import {CoreModule} from './core.module';
     BrowserModule,
     SharedModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot({shoopinglist:ShoopingListReducer})
   ],
   declarations: [
     AppComponent,
---
+++ src/app/shooping-list/Store/shooping-list.reducer.ts
@@ -0,0 +1,16 @@
+import { ingredient } from "../../shared/ingredient.model";
+import * as shoopinglistaction  from './shooping-list.actions';
+
+const initialstate = {
+  ingredients: [new ingredient("Banana", 10), new ingredient("Apple", 20)]
+};
+
+export function ShoopingListReducer(state = initialstate, action: shoopinglistaction.AddIngredient) {
+  switch (action.type) {
+    case shoopinglistaction.ADD_INGREDIENT:
+    return {
+      ...state,
+      ingredients:[...state.ingredients,action.payload]
+    }
+  }
+}
