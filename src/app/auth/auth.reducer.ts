f9e6ed6156b96590938ee0b14ab461bba7de732d
sonukuntal <sonu.savera.kuntal@gmail.com>
2019-12-24 15:04:05 +0530

    1 file modified, 1 added

--- src/app/auth/auth.reducer.ts
+++ src/app/auth/auth.reducer.ts
@@ -1,5 +1,6 @@
 import { Store } from "@ngrx/Store";
 import { User } from "./user.model";
+import * as AuthActions from "./auth.action";
 
 export interface State {
   user: User;
@@ -9,6 +10,32 @@ const initialState: State = {
   user: null
 };
 
-export function AuthReducer(state = initialState, action) {
+export function AuthReducer(
+  state = initialState,
+  action: AuthActions.AuthActions
+) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      const newuser = new User(
+        action.payload.email,
+        action.payload.userID,
+        action.payload.token,
+        action.payload.expirationDate
+      );
+      return {
+        ...state,
+        user:newuser
+      };
+
+    case AuthActions.LOGOUT: {
+      return {
+        ...state,
+        user:null
+      };
+    }
+
+    default:
+      return state;
+  }
   return state;
 }
