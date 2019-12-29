02af01d6ab669d94539c30c3750a17232c5dec8b
sonukuntal <sonu.savera.kuntal@gmail.com>
2019-12-29 16:10:21 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 import { StoreModule } from "@ngrx/store";
 import {EffectsModule} from '@ngrx/effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -21,7 +23,9 @@ import {AuthEffects} from './auth/auth.effects';
     AppRoutingModule,
     CoreModule,
     StoreModule.forRoot(authReducer.AppReducer),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({}),
+    StoreRouterConnectingModule.forRoot() 
   ],
   declarations: [AppComponent, HeaderComponent, ErrorpageComponent],
   bootstrap: [AppComponent],
